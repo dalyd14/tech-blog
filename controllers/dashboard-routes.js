22979f58fb2043cd9690c994c35ee4dc5e8fb929
David Daly <dalyd14@gmail.com>
2021-02-10 10:56:37 -0500

    edit post fields are populated

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,50 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../model')
 
 router.get('/', (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'title', 'created_at']
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }))
+        console.log(posts)
+        res.render('dashboard', {
+            posts,
+            session: req.session
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    });
+})
 
+router.get('/edit/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_content', 'user_id']
+    })
+    .then(dbPostData => {
+        if(req.session.user_id === dbPostData.user_id) {
+            const post = dbPostData.get({ plain: true })
+            console.log(post)
+            res.render('edit-post', {
+                post,
+                session: req.session
+            })            
+        } else {
+            // res.status(400).json({message: 'This is not your post to edit'})
+            res.redirect('/dashboard')
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    });
 })
 
 module.exports = router
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,14 +35,14 @@ router.get('/', (req, res) => {
 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
-        res.redirect('/')
+        res.redirect('/dashboard')
         return
     }
     res.render('login')
 })
 router.get('/signup', (req, res) => {
     if(req.session.loggedIn) {
-        res.redirect('/')
+        res.redirect('/dashboard')
         return
     }
     res.render('signup')
